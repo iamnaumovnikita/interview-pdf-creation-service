c5d57b8ba7cf05c1badb9d822f4a699e5dbf3795
Martin Ayasse <mayasse@aligntech.com>
2025-04-14 12:44:12 +0200

    Add project files.

---
+++ Features/RenderCityMap/RenderCityMapEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Interview.PdfCreation.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Playwright;
+using System.Net.Mime;
+
+namespace Interview.PdfCreation.Features.RenderCityMap;
+
+public static class RenderCityMapEndpoint
+{
+    public static async Task<FileContentHttpResult> Execute(string city, IBrowserWrapper browserWrapper)
+    {
+        var page = await browserWrapper.GetPage();
+        await page.GotoAsync("https://consent.google.com/m?continue=https://www.google.com/maps/place/" + city + "&gl=DE&m=0&pc=m&uxe=eomtm&cm=2&hl=en&src=1");
+        await page.GetByRole(AriaRole.Heading, new() { Name = city }).ClickAsync();
+
+        var pdf = await page.PdfAsync(new()
+        {
+            PrintBackground = true
+        });
+        await page.CloseAsync();
+
+        return TypedResults.File(pdf, MediaTypeNames.Application.Pdf, city + ".pdf");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Interview.PdfCreation.Features.RenderCityMap;
+using Interview.PdfCreation.Services;
+
+Console.WriteLine("Installing Playwright Browsers...");
+var exitCode = Microsoft.Playwright.Program.Main(new[] { "install", "chromium" });
+if (exitCode != 0)
+{
+    throw new Exception($"Playwright exited with code {exitCode}");
+}
+Console.WriteLine("Playwright Browsers installed successfully.");
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// services
+builder.Services.AddSingleton<IBrowserWrapper, BrowserWrapper>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/api/render/city", RenderCityMapEndpoint.Execute)
+.WithOpenApi();
+
+app.Run();
+
